e64dce93253b84a4b84e4d26be4b1b5bc7c875d5
Fílip Anselmo <filipanselmoalvesdasilva870@gmail.com>
2023-07-10 12:24:48 -0300

    feat: Add rotas do  usuário

--- src/index.js
+++ src/index.js
@@ -4,5 +4,18 @@ const app = express()
 
 app.use(express.json())
 
+const users = []
+
+app.post("/users", (request, response) => {
+    // const { id, name, username, todos } = request.body => forma alternativa
+    const body = request.body
+    users.push(body)
+    return response.json(body)
+})
+
+app.get("/users", (request, response) => {
+    return response.json(users)
+})
+
 
 app.listen(3030)
